7e5b6fd28cd8d601299be7f2392d7bb70a825958
Lukas Seeger <lseeger@.42heilbronn.de>
2025-11-19 14:04:25 +0100

    first casting

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -1,5 +1,15 @@
 #include "ScalarConverter.hpp"
 
+static void print(char c, int i, float f, double d) {
+    if (std::isprint(c))
+        std::cout << "char: " << c << std::endl;
+    else
+        std::cout << "char: Non displayable" << std::endl;
+    std::cout << "int: " << i << std::endl;
+    std::cout << "float: " << f << std::endl;
+    std::cout << "double: " << d << std::endl;
+}
+
 void ScalarConverter::convert(const std::string& literal)
 {
     char c;
@@ -12,17 +22,41 @@ void ScalarConverter::convert(const std::string& literal)
         i = static_cast<int>(c);
         f = static_cast<float>(c);
         d = static_cast<double>(c);
+        print(c, i, f, d);
+        return;
     }
-    else {
-        throw std::invalid_argument("[ScalarConverter::convert] Invalid literal");
-    }
+    try
+    {
+        i = std::stoi(literal);
+        c = static_cast<char>(i);
+        f = static_cast<float>(i);
+        d = static_cast<double>(i);
+        print(c, i, f, d);
+        return;
+    } catch(const std::exception& e) {}
 
-    if (std::isprint(c))
-        std::cout << "char: " << c << std::endl;
-    else
-        std::cout << "char: Non displayable" << std::endl;
-    std::cout << "int: " << i << std::endl;
-    std::cout << "float: " << f << std::endl;
-    std::cout << "double: " << d << std::endl;
+    try
+    {
+        f = std::stof(literal);
+        c = static_cast<char>(f);
+        i = static_cast<int>(f);
+        d = static_cast<double>(f);
+        print(c, i, f, d);
+        return;
+    }
+    catch(const std::exception& e) {}
+
+    try
+    {
+        d = std::stod(literal);
+        c = static_cast<char>(d);
+        i = static_cast<int>(d);
+        f = static_cast<float>(d);
+        print(c, i, f, d);
+        return;
+    }
+    catch(const std::exception& e) {}
+    
+    // Base Case
+    throw std::invalid_argument("[ScalarConverter::convert] Invalid literal");
 }
-
