5bb02b8dbf760f36f56238509ba0274468611405
Lukas Seeger <lseeger@.42heilbronn.de>
2025-11-19 13:24:52 +0100

    Base Scalar Converter class

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class ScalarConverter
+{
+private:
+
+public:
+        ScalarConverter() = delete;
+        ScalarConverter(const ScalarConverter& other) = delete;
+        ScalarConverter& operator=(const ScalarConverter& other) = delete;
+        ~ScalarConverter() = delete;
+};
